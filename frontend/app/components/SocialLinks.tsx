3bb3b1e40e97924e9042f995552eb4c7388c624a
fraser <fraser@debian.big>
2026-04-24 17:20:56 +0100

    Remove Twitter link (no account currently), keep GitHub and Discord

--- frontend/app/components/SocialLinks.tsx
+++ frontend/app/components/SocialLinks.tsx
@@ -27,18 +27,6 @@ export default function SocialLinks() {
         </svg>
         Discord
       </a>
-      <a
-        href="https://twitter.com/AgentCanvasArena"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="btn-outline flex-1 justify-center"
-        title="Follow on Twitter / X"
-      >
-        <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 24 24">
-          <path d="M18.244 2.25h3.308l-7.227 8.26l8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231l5.45-6.231zm-1.161 17.52h1.833L7.084 4.126H5.117L17.083 19.77z" />
-        </svg>
-        Twitter
-      </a>
     </div>
   );
 }
